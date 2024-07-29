63ea2c6f1a4a4baa7137c2d910580264d2f999db
jim-low <robil12380@gmail.com>
2024-07-30 00:58:58 +0800

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    function handleRickRoll() {
+        window.location.href = "https://www.youtube.com/watch?v=uHgt8giw1LY"
+    }
+
+    return (
+        <div className="app">
+            <h1>Hello world, Welcome to Jim's React Page</h1>
+            <button onClick={handleRickRoll}>click me</button>
+        </div>
+    );
 }
 
 export default App;
