391a81fc06f68b1c00889ad9ee1b66e583a2b0ad
jim-low <robil12380@gmail.com>
2024-07-31 20:14:08 +0800

    refactor: change file name to App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+    return (
+        <div className="app">
+            <h1>Hello world, Welcome to Jim's React Page</h1>
+            <button>click me</button>
+        </div>
+    );
+}
+
+export default App;
