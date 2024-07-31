d1d2501deaac2596c49b165d8a0962fbc54bfc73
jim-low <robil12380@gmail.com>
2024-07-31 20:14:28 +0800

    feat: install Radix UI package

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './main';
+import App from './App';
+import '@radix-ui/themes/styles.css';
+import { Theme } from '@radix-ui/themes';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <React.StrictMode>
-        <App />
+        <Theme>
+            <App />
+        </Theme>
     </React.StrictMode>
 );
